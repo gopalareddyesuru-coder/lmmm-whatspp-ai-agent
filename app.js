5581432c7498421237523d316e625291c1ab7cf9
gopalareddyesuru-coder <gopalareddyesuru@gmail.com>
2026-09-14 09:32:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -35,7 +35,6 @@ app.get('/health', (_req, res) => {
     verify_token_configured: Boolean(VERIFY_TOKEN)
   });
 });
-const express = require("express");
 const app = express();
 
 // your other middleware here
